3819fa57127cd5c8dc87d1150f961f2e3267e923
Gabriel Hardt Klipe <gabrielhardtklipegokusuper@gmail.com>
2026-06-06 22:39:34 -0300

    Estruturando as paginas

---
+++ src/components/CardCarro.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  id: String,
+  nome: String,
+  marca: String,
+  ano: String,
+  preco: String,
+  imagem: String,
+  rota: String
+})
+</script>
+
+<template>
+  <div class="card-carro">
+    <div class="card-imagem">
+      <img :src="imagem" :alt="nome" />
+    </div>
+    <div class="card-conteudo">
+      <span class="marca-tag">{{ marca }}</span>
+      <h3>{{ nome }}</h3>
+      <p class="ano-texto">Ano: {{ ano }}</p>
+      <div class="card-footer">
+        <span class="preco-texto">R$ {{ preco }}</span>
+        <router-link :to="rota" class="btn-detalhes">Ver Mais</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped src="../css/CardCars.css"></style>
