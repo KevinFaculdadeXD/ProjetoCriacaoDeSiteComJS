aeb3a834b5509be50facb655d61db8d09e582a82
Kevin Richardt <engs-kevinrichardt@camporeal.edu.br>
2026-06-06 23:11:10 -0300

    inserindo o Twingo e o fusca

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="hero-section">
-    <h1>Bem-vindo à XDCars Motors</h1>
+    <h1>Bem-vindo à XD Carros Motors</h1>
     <p>A sua nova referência em veículos exclusivos, esportivos e de alta performance.</p>
     <div class="actions">
       <router-link to="/catalogo" class="btn-primary-home">Ver Catálogo Completo</router-link>
